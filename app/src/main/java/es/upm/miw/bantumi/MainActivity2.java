30d6dd3715324391e3df13f349435b0948a55352
Wael Louati <waelwzwz@gmail.com>
2022-11-02 13:57:18 -0800

    Added Main activity 2 Launcher

---
+++ app/src/main/java/es/upm/miw/bantumi/MainActivity2.java
@@ -0,0 +1,37 @@
+package es.upm.miw.bantumi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+
+    public void New(View v)
+    {
+        // TODO Auto-generated method stub
+        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(i);
+
+    }
+     public void History(View v)
+    {
+        // TODO Auto-generated method stub
+        Intent i = new Intent(getApplicationContext(),MainActivity2.class);
+        startActivity(i);
+
+    }
+    public void Exit(View v){
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
+}
